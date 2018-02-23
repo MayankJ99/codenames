fdb6ffea74495b94f5f30c8e6f74bde1ced55577
Minseo Kim <Minseo Kim@LAPTOP-9KRPT66G>
2018-02-22 20:48:30 -0500

    Location Class

---
+++ src/Code/Location.java
@@ -0,0 +1,39 @@
+package Code;
+
+public class Location {
+	private String codename;
+	private String Person;
+	private	boolean Revealed;
+	
+	public Location() {
+		codename = "";
+		Person = "";
+		Revealed = false;
+	}
+
+	public void setCodename(String c) {
+		codename = c;
+	}
+		
+	public String getCodename() {
+		return codename;
+	}
+	
+	public void setPerson(String p) {
+		Person = p;
+	}
+
+	public String getPerson() {
+		return Person;
+	}
+
+	public void setRevealed(boolean r) {
+		Revealed = r;
+	}
+	
+	public boolean getRevealed() {
+		return Revealed;
+	}
+
+
+}
