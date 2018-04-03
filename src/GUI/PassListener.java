dab96392d5f262282f3e7e6a6c148d0cb31e2c4d
Minseo Kim <Minseo Kim@LAPTOP-9KRPT66G>
2018-04-03 18:09:16 -0400

    update() else{}, passButton

---
+++ src/GUI/PassListener.java
@@ -0,0 +1,27 @@
+package GUI;
+
+import Code.Board;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+
+public class PassListener implements ActionListener {
+	private GUI gui;
+	private Board board;
+
+	public PassListener(GUI g, Board b) {
+		this.gui = g;
+		this.board = b;
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		this.gui.newTurn();
+		this.board.setRedTurn(!this.board.getRedTurn());
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+}
