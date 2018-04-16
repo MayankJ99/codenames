196ea7a0df1735ca5dfcd0f29a168ea72cbf7c90
Daniel Walsh <dwalsh3@buffalo.edu>
2018-04-15 23:08:12 -0400

    Added javadoc

--- src/GUI/CloseListener.java
+++ src/GUI/CloseListener.java
@@ -3,8 +3,19 @@ package GUI;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * This class is an action listener for the Close JMenu item and Close button, which close the JFrame.
+ * 
+ * @author Mayank Jha
+ * @author Minseo Kim
+ * @author Juan Mendoza
+ * @author Daniel Walsh
+ */
 public class CloseListener implements ActionListener {
 
+	/**
+	 * Calls System.exit(0) when a close menu item or button are clicked, exiting the JFrame.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		System.exit(0);
