f7b0c3e316173e19605cbd15aa0d4ff7c5559543
Daniel Walsh <dwalsh3@buffalo.edu>
2018-04-15 22:57:42 -0400

    Added javadoc

--- src/GUI/NewGameListener.java
+++ src/GUI/NewGameListener.java
@@ -5,17 +5,34 @@ import java.awt.event.ActionListener;
 
 import Code.Board;
 
+/**
+ * This class is an action listener for the new game menu item and JButton.
+ * 
+ * @author Mayank Jha
+ * @author Minseo Kim
+ * @author Juan Mendoza
+ * @author Daniel Walsh
+ */
 public class NewGameListener implements ActionListener{
 
+	/**
+	 * The model which contains the event handler.
+	 */
 	private Board _b;
+	
+	/**
+	 * Constructor
+	 * @param _board A reference to the model for the current game which contains the event handler.
+	 */
 	 public NewGameListener(Board _board) {
 		 _b=_board;
 	 }
 	
+	 /**
+	  * Calls the event handler in _b when a new game is selected during game play.
+	  */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-	    
 		_b.gameStart();
 	}
 
