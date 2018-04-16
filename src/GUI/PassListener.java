b63ef58a7394b07a5097bff3742c19cf780d01e9
Daniel Walsh <dwalsh3@buffalo.edu>
2018-04-15 22:40:43 -0400

    Added javadoc

--- src/GUI/PassListener.java
+++ src/GUI/PassListener.java
@@ -5,23 +5,36 @@ import Code.Board;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JTextField;
-
+/**
+ * This class is a button listener for the Pass button, used when a team wants to end their turn.
+ * 
+ * @author Mayank Jha
+ * @author Minseo Kim
+ * @author Juan Mendoza
+ * @author Daniel Walsh
+ */
 public class PassListener implements ActionListener {
-	private GUI gui;
+	
+	/**
+	 * The model for the current game
+	 */
 	private Board board;
 
+	/**
+	 * Constructor
+	 * @param g The GUI for the current game
+	 * @param b The Board for the current game
+	 */
 	public PassListener(GUI g, Board b) {
-		this.gui = g;
 		this.board = b;
 	}
+	
+	/**
+	 * Calls the board handler method in response to the Pass button being clicked.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		this.board.passListenerEvent();
-//		this.board.setRedTurn(!this.board.getRedTurn());
-		
-		// TODO Auto-generated method stub
-		
 	}
 
 }
