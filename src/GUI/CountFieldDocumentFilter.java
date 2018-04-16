321e19360d580947ce57dc32a29419c7dba3bc84
Daniel Walsh <dwalsh3@buffalo.edu>
2018-04-15 23:04:10 -0400

    Added javadoc

--- src/GUI/CountFieldDocumentFilter.java
+++ src/GUI/CountFieldDocumentFilter.java
@@ -6,9 +6,26 @@ import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DocumentFilter;
 
+/**
+ * This class is a DocumentFilter for the count entry JTextField in codenames. It prevents invalid entries from being entered.
+ * 
+ * @author Mayank Jha
+ * @author Minseo Kim
+ * @author Juan Mendoza
+ * @author Daniel Walsh
+ */
 public class CountFieldDocumentFilter extends DocumentFilter {
+	
+	/**
+	 * Constructor
+	 */
 	public CountFieldDocumentFilter() {}
 	
+	/**
+	 * Examines the String of characters entered into the count JTextField via the insertString method. Any characters that are not '0' 
+	 * through '9' are removed, and any remaining characters are passed to the JTextField Document. They will then appear on the screen.
+	 * If no characters are to be passed to the Document, no valid characters were entered, and the system will beep to indicate invalid inputs.
+	 */
 	@Override
 	public void insertString(FilterBypass fb, int offset, String str, AttributeSet attr) throws BadLocationException {
 		String newString = new String();
@@ -21,6 +38,11 @@ public class CountFieldDocumentFilter extends DocumentFilter {
 			Toolkit.getDefaultToolkit().beep();
 	}
 	
+	/**
+	 * Examines the String of characters entered into the count JTextField via the replace method. Any characters that are not '0' 
+	 * through '9' are removed, and any remaining characters are passed to the JTextField Document. They will then appear on the screen.
+	 * If no characters are to be passed to the Document, no valid characters were entered, and the system will beep to indicate invalid inputs.
+	 */
 	@Override
 	public void replace(FilterBypass fb, int offset, int length, String str, AttributeSet attr) throws BadLocationException {
 		String newString = new String();
