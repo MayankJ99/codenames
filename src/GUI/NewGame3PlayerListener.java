3111ddee637c3b0ef435d9f087a084e535118f2c
MostJuanted <juan.j.mendoza513@gmail.com>
2018-04-28 17:42:55 -0400

    3 player button Listener

---
+++ src/GUI/NewGame3PlayerListener.java
@@ -0,0 +1,39 @@
+package GUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import Code.Board;
+
+/**
+ * This class is an action listener for the new game menu item and JButton.
+ * 
+ * @author Mayank Jha
+ * @author Minseo Kim
+ * @author Juan Mendoza
+ * @author Daniel Walsh
+ */
+public class NewGame3PlayerListener implements ActionListener{
+
+	/**
+	 * The model which contains the event handler.
+	 */
+	private Board _b;
+	
+	/**
+	 * Constructor
+	 * @param _board A reference to the model for the current game which contains the event handler.
+	 */
+	 public NewGame3PlayerListener(Board _board) {
+		 _b=_board;
+	 }
+	
+	 /**
+	  * Calls the event handler in _b when a new game is selected during game play.
+	  */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		_b.gameStart_3Team();
+	}
+
+}
