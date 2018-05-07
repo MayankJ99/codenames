8d0c6f92e93ab0c68adeef7d80a7a357360ae5e2
Daniel Walsh <dwalsh3@buffalo.edu>
2018-05-06 20:51:56 -0400

    Added javadoc to Observer interface

--- src/GUI/Observer.java
+++ src/GUI/Observer.java
@@ -1,5 +1,17 @@
 package GUI;
 
+/**
+ * Interface for use in Java GUI applications.
+ * 
+ * @author Mayank Jha
+ * @author Minseo Kim
+ * @author Juan Mendoza
+ * @author Daniel Walsh
+ */
 public interface Observer {
+	
+	/**
+	 * Prompts a GUI to repaint itself to reflect changes in its content
+	 */
 	public void update();
 }
